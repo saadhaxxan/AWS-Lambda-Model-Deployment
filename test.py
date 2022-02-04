d8a0a29ebf765404f18cb5dedaad13efecf8fa4b
Saad Hassan <saad.haxxan786@gmail.com>
2022-02-04 19:16:19 +0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import tensorflow as tf
-import cv2
-import tflite_runtime.interpreter as tflite
-
-
-interpreter = tflite.Interpreter(model_path="decrypted.tflite")
-interpreter.allocate_tensors()
